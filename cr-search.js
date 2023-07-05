dc39d70388788f6cef7a016171d215bf0abe042b
T2da <57564981+T2da@users.noreply.github.com>
2023-07-05 23:31:16 +0300

    cr-search.js

--- cr-search.js
+++ cr-search.js
@@ -1,94 +1,88 @@
-(function($) {
-    $.fn.crSearch = function(options) {
-        let settings = $.extend({
+(function(n) {
+    n.fn.crSearch = function(t) {
+        const i = n.extend({
             minCharsToShowMenu: 3,
             object: [],
             maxResults: 5,
-            placeholderText: "Enter a query",
-            noResultsText: "No results",
-            showImage: true,
-            useNaturalLanguage: false,
+            placeholderText: "Введите запрос",
+            noResultsText: "Нет результатов",
+            showImage: !0,
+            useNaturalLanguage: !1,
             onSelect: null
-        }, options);
-
+        }, t);
         return this.each(function() {
-            let input = $(this);
-            let suggestionsContainer = $('<div class="suggestions-container"></div>');
-            input.attr("placeholder", settings.placeholderText);
-            let clearIcon = $('<i class="clear-icon fas fa-times"></i>');
-            input.after(clearIcon);
+            function f(n, t, i) {
+                const r = new Fuse(t, {
+                    keys: ["text"]
+                });
+                const u = r.search(n);
+                const f = u.slice(0, i);
+                return f.map(n => n.item)
+            }
 
-            clearIcon.on("click", function() {
-                input.val("").focus();
-                suggestionsContainer.empty().hide();
-                clearIcon.hide();
+            function e(n, t) {
+                return t.filter(function(t) {
+                    return t.text.toLowerCase().includes(n.toLowerCase())
+                })
+            }
+            const $input = n(this);
+            const $suggestionsContainer = n('<div class="suggestions-container"></div>');
+            $input.attr("placeholder", i.placeholderText);
+            const $clearIcon = n('<i class="clear-icon fas fa-times"></i>');
+            $input.after($clearIcon);
+            $clearIcon.on("click", function() {
+                $input.val("").focus();
+                $suggestionsContainer.empty().hide();
+                $clearIcon.hide()
             });
-
-            input.on("input", function() {
-                let query = input.val().trim();
-                if (suggestionsContainer.empty().hide(), clearIcon.toggle(query.length > 0), query.length >= settings.minCharsToShowMenu) {
+            $input.on("input", function() {
+                const query = $input.val().trim();
+                if ($suggestionsContainer.empty().hide(), $clearIcon.toggle(query.length > 0), query.length >= i.minCharsToShowMenu) {
                     let results;
-                    if (settings.useNaturalLanguage) {
-                        results = fuzzySearch(query, settings.object, settings.maxResults);
+                    if (i.useNaturalLanguage) {
+                        results = f(query, i.object, i.maxResults);
                     } else {
-                        results = filterResults(query, settings.object, settings.maxResults);
+                        results = e(query, i.object, i.maxResults);
                     }
-
                     if (results.length > 0) {
-                        $.each(results, function(index, result) {
-                            let suggestion = $('<div class="suggestion"></div>');
-                            if (settings.showImage) {
-                                let image = $('<img class="suggestion-image" src="' + result.imageUrl + '">');
-                                suggestion.append(image);
+                        n.each(results, function(index, item) {
+                            const $suggestion = n('<div class="suggestion"></div>');
+                            if (i.showImage) {
+                                const $image = n('<img class="suggestion-image" src="' + item.imageUrl + '">');
+                                $suggestion.append($image);
                             }
-                            let text = $('<div class="suggestion-text">' + result.text + "</div>");
-                            suggestion.append(text);
-
-                            suggestion.on("click", function() {
-                                input.val(result.text);
-                                suggestionsContainer.empty().hide();
-                                page.getComponent(el).setValue(123)
+                            const $text = n('<div class="suggestion-text">' + item.text + '</div>');
+                            $suggestion.append($text);
+                            $suggestion.on("click", function() {
+                                $input.val(item.text);
+                                $suggestionsContainer.empty().hide();
+                                if (typeof i.onSelect === 'function') {
+                                    i.onSelect(item); // Вызов метода при выборе пункта
+                                }
                             });
-
-                            suggestionsContainer.append(suggestion);
+                            $suggestionsContainer.append($suggestion);
                         });
-                        suggestionsContainer.insertAfter(input).show();
+                        $suggestionsContainer.insertAfter($input).show();
                     } else {
-                        let noResults = $('<div class="no-results">' + settings.noResultsText + "</div>");
-                        suggestionsContainer.append(noResults);
-                        suggestionsContainer.insertAfter(input).show();
+                        const $noResults = n('<div class="no-results">' + i.noResultsText + '</div>');
+                        $suggestionsContainer.append($noResults);
+                        $suggestionsContainer.insertAfter($input).show();
                     }
                 }
             });
-
-            input.on("keyup", function(event) {
-                let query = input.val().trim();
-                if (query.length === 0 && event.keyCode === 8) {
-                    suggestionsContainer.empty().hide();
-                    clearIcon.hide();
+            $input.on("keyup", function(e) {
+                const query = $input.val().trim();
+                if (query.length === 0 && e.keyCode === 8) {
+                    $suggestionsContainer.empty().hide();
+                    $clearIcon.hide();
                 }
             });
-
-            $(document).on("click", function(event) {
-                if (!$(event.target).closest(".suggestions-container").length && !$(event.target).is(input)) {
-                    suggestionsContainer.empty().hide();
-                    clearIcon.hide();
+            n(document).on("click", function(e) {
+                if (!n(e.target).closest(".suggestions-container").length && !n(e.target).is($input)) {
+                    $suggestionsContainer.empty().hide();
+                    $clearIcon.hide();
                 }
             });
         });
-    }
-
-    function fuzzySearch(query, items, maxResults) {
-        let fuse = new Fuse(items, {
-            keys: ["text"]
-        });
-        let searchResults = fuse.search(query);
-        return searchResults.slice(0, maxResults).map(result => result.item);
-    }
-
-    function filterResults(query, items, maxResults) {
-        return items.filter(function(item) {
-            return item.text.toLowerCase
-        }).slice(0, maxResults);
-    }
+    };
 })(jQuery);
