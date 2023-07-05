116411f99dc3da9dbfaf93b1caaa9c858e7e69e3
T2da <57564981+T2da@users.noreply.github.com>
2023-07-05 23:27:44 +0300

    cr-search.js

--- cr-search.js
+++ cr-search.js
@@ -1,1 +1,88 @@
-(function(n){function t(n,t,i){let r=new Fuse(t,{keys:["text"]}),u=r.search(n);return u.slice(0,i).map(n=>n.item)}function i(n,t,i){return t.filter(function(n){return n.text.toLowerCase}).slice(0,i)}n.fn.crSearch=function(r){let u=n.extend({minCharsToShowMenu:3,object:[],maxResults:5,placeholderText:"Enter a query",noResultsText:"No results",showImage:!0,useNaturalLanguage:!1,onSelect:null},r);return this.each(function(){let r=n(this),f=n('<div class="suggestions-container"><\/div>');r.attr("placeholder",u.placeholderText);let e=n('<i class="clear-icon fas fa-times"><\/i>');r.after(e);e.on("click",function(){r.val("").focus();f.empty().hide();e.hide()});r.on("input",function(){let o=r.val().trim();if(f.empty().hide(),e.toggle(o.length>0),o.length>=u.minCharsToShowMenu){let e;if(e=u.useNaturalLanguage?t(o,u.object,u.maxResults):i(o,u.object,u.maxResults),e.length>0)n.each(e,function(t,i){let e=n('<div class="suggestion"><\/div>');if(u.showImage){let t=n('<img class="suggestion-image" src="'+i.imageUrl+'">');e.append(t)}let o=n('<div class="suggestion-text">'+i.text+"<\/div>");e.append(o);e.on("click",function(){if(r.val(i.text),f.empty().hide(),typeof u.onSelect=="function")u.onSelect(i)});f.append(e)}),f.insertAfter(r).show();else{let t=n('<div class="no-results">'+u.noResultsText+"<\/div>");f.append(t);f.insertAfter(r).show()}}});r.on("keyup",function(n){let t=r.val().trim();t.length===0&&n.keyCode===8&&(f.empty().hide(),e.hide())});n(document).on("click",function(t){n(t.target).closest(".suggestions-container").length||n(t.target).is(r)||(f.empty().hide(),e.hide())})})}})(jQuery);
+(function(n) {
+    n.fn.crSearch = function(t) {
+        const i = n.extend({
+            minCharsToShowMenu: 3,
+            object: [],
+            maxResults: 5,
+            placeholderText: "Введите запрос",
+            noResultsText: "Нет результатов",
+            showImage: !0,
+            useNaturalLanguage: !1,
+            onSelect: null
+        }, t);
+        return this.each(function() {
+            function f(n, t, i) {
+                const r = new Fuse(t, {
+                    keys: ["text"]
+                });
+                const u = r.search(n);
+                const f = u.slice(0, i);
+                return f.map(n => n.item)
+            }
+
+            function e(n, t) {
+                return t.filter(function(t) {
+                    return t.text.toLowerCase().includes(n.toLowerCase())
+                })
+            }
+            const $input = n(this);
+            const $suggestionsContainer = n('<div class="suggestions-container"></div>');
+            $input.attr("placeholder", i.placeholderText);
+            const $clearIcon = n('<i class="clear-icon fas fa-times"></i>');
+            $input.after($clearIcon);
+            $clearIcon.on("click", function() {
+                $input.val("").focus();
+                $suggestionsContainer.empty().hide();
+                $clearIcon.hide()
+            });
+            $input.on("input", function() {
+                const query = $input.val().trim();
+                if ($suggestionsContainer.empty().hide(), $clearIcon.toggle(query.length > 0), query.length >= i.minCharsToShowMenu) {
+                    let results;
+                    if (i.useNaturalLanguage) {
+                        results = f(query, i.object, i.maxResults);
+                    } else {
+                        results = e(query, i.object, i.maxResults);
+                    }
+                    if (results.length > 0) {
+                        n.each(results, function(index, item) {
+                            const $suggestion = n('<div class="suggestion"></div>');
+                            if (i.showImage) {
+                                const $image = n('<img class="suggestion-image" src="' + item.imageUrl + '">');
+                                $suggestion.append($image);
+                            }
+                            const $text = n('<div class="suggestion-text">' + item.text + '</div>');
+                            $suggestion.append($text);
+                            $suggestion.on("click", function() {
+                                $input.val(item.text);
+                                $suggestionsContainer.empty().hide();
+                                if (typeof i.onSelect === 'function') {
+                                    i.onSelect(item); // Вызов метода при выборе пункта
+                                }
+                            });
+                            $suggestionsContainer.append($suggestion);
+                        });
+                        $suggestionsContainer.insertAfter($input).show();
+                    } else {
+                        const $noResults = n('<div class="no-results">' + i.noResultsText + '</div>');
+                        $suggestionsContainer.append($noResults);
+                        $suggestionsContainer.insertAfter($input).show();
+                    }
+                }
+            });
+            $input.on("keyup", function(e) {
+                const query = $input.val().trim();
+                if (query.length === 0 && e.keyCode === 8) {
+                    $suggestionsContainer.empty().hide();
+                    $clearIcon.hide();
+                }
+            });
+            n(document).on("click", function(e) {
+                if (!n(e.target).closest(".suggestions-container").length && !n(e.target).is($input)) {
+                    $suggestionsContainer.empty().hide();
+                    $clearIcon.hide();
+                }
+            });
+        });
+    }
+})(jQuery);
